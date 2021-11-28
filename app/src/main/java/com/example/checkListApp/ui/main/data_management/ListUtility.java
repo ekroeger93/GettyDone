1f23a5ecef2fdd227690d2aca5f1c2720c232a92
eric <kroeger_e@yahoo.com>
2021-11-28 10:17:54 -0500

    TaDone alpha 0.74 rever

--- app/src/main/java/com/example/checkListApp/ui/main/data_management/ListUtility.java
+++ app/src/main/java/com/example/checkListApp/ui/main/data_management/ListUtility.java
@@ -8,7 +8,7 @@ import androidx.annotation.RequiresApi;
 import com.example.checkListApp.timer.TimeState;
 import com.example.checkListApp.ui.main.entries.Entry;
 import com.example.checkListApp.ui.main.entries.Spacer;
-import com.example.checkListApp.ui.main.entry_management.ListComponent.ToggleSwitchOrdering;
+import com.example.checkListApp.ui.main.EntryManagement.ListComponent.ToggleSwitchOrdering;
 
 import java.util.ArrayList;
 
@@ -108,48 +108,28 @@ public final class ListUtility {
 
         int sum = 0;
 
-    //    TimeState state = new TimeState(0);
-
         for(Entry viewModel:list){
 
-            //TODO: CHECK THIS
-                viewModel.setNumberValueTime(viewModel.timeTemp);
+            viewModel.setNumberValueTime(viewModel.timeTemp);
 
-                int value = viewModel.getNumberValueTime();
-
-                if (viewModel.getNumberValueTime() != 0) {
-                      sum += value;
-                }
-
-                Log.d("summationT", "n= " + value);
+            if(viewModel.numberValueTime !=0)
+                sum+= new TimeState(viewModel.numberValueTime).getTimeNumberValue();
 
         }
-
-        Log.d("summationT","sum= "+sum);
 
         return sum;
     }
 
     public static ArrayList<Entry> accumulation(ArrayList<Entry> list){
 
-//        for(int i = 0; i < list.size(); i++){
-//
-//            if(i != 0) {
-//                list.get(i).setTimeAcclimated(
-//                        list.get(i - 1).timeAccumulated);
-//            }
-//
-//            Log.d("testTime", i+" = "+list.get(i).timeAccumulated);
-//        }
+        for(int i = 0; i < list.size(); i++){
 
-        for(int i = 1; i < list.size(); i++){
-
-            if(i != 1 && list.get(i).numberValueTime !=0) {
+            if(i != 0) {
                 list.get(i).setTimeAcclimated(
-                        list.get(i - 1).numberValueTime);
+                        list.get(i - 1).timeAccumulated);
             }
 
-            Log.d("testTimerTest", "acc:::  " + list.get(i).timeAccumulated);
+            Log.d("testTime", i+" = "+list.get(i).timeAccumulated);
         }
 
         return list;
